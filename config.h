cdc8628891471e0337dc584fb58ecfa29de1cc66
johnnymadigan <silkjay@icloud.com>
2021-09-24 22:03:27 +1000

    +config file for client

---
+++ config.h
@@ -0,0 +1,13 @@
+/*******************************************************
+ * @file    config.h
+ * @brief   Configuration file so clients can easily recompile 
+ *          the software pieces to accomodate different carparks
+ * @author  Johnny Madigan
+ * @date    September 2021
+ ******************************************************/
+#pragma once /* please do not touch this line */
+
+#define ENTRANCES 5
+#define EXITS 5
+#define LEVELS 5
+#define CAPACITY 20 /* cars per level */
