7b943a5613b01dcbbb028df110fc4392eadb4bf8
johnnymadigan <silkjay@icloud.com>
2021-09-26 22:06:30 +1000

    added boom gate logic

--- src-manager/manager.c
+++ src-manager/manager.c
@@ -130,7 +130,7 @@ void *manage_entrance(void *args) {
         pthread_mutex_lock(&en->sensor.lock);
         /* wait until LPR hardware reads in a number plate */
         while (strcmp(en->sensor.plate, "") == 0) {
-            puts("waiting for simulation to read plate");
+            //puts("waiting for simulation to read plate");
 
             pthread_cond_wait(&en->sensor.condition, &en->sensor.lock);
         }
@@ -138,14 +138,12 @@ void *manage_entrance(void *args) {
         /* validate plate, locking the hash table as it is global */
         pthread_mutex_lock(&plates_ht_lock);
         bool authorised = hashtable_find(plates_ht, en->sensor.plate);
+
         pthread_mutex_unlock(&plates_ht_lock);
 
         /* after validating, clear the info sign */
         strcpy(en->sensor.plate, "");
         pthread_mutex_unlock(&en->sensor.lock);
-
-
-
 
         /* update infosign to direct the car to either enter or leave */
         pthread_mutex_lock(&en->sign.lock);
@@ -154,26 +152,41 @@ void *manage_entrance(void *args) {
         if (!authorised) {
             en->sign.display = 'X';
 
-        } else if (current_cap < (CAPACITY * LEVELS)) {
+        } else if (current_cap > (CAPACITY * LEVELS)) {
             en->sign.display = 'F';
 
-        } else {
+        } else if (authorised) {
             current_cap++; /* reserve space as car is authorised */
             en->sign.display = '1'; /* goto floor 1 for now */
+            
+            puts("ending here tonight, need to fix authorised");
 
+            pthread_mutex_lock(&en->gate.lock);
+            /* if closed? raise */
+            if (en->gate.status == 'C') {
+                puts("about to raise gate");
+                en->gate.status = 'R';
+            }
+            pthread_mutex_unlock(&en->gate.lock);
+            pthread_cond_signal(&en->gate.condition);
         }
-        puts("validated");
+        /* unlock current capacity, sign, and signal simulator */
         pthread_mutex_unlock(&current_cap_lock);
         pthread_mutex_unlock(&en->sign.lock);
-
         pthread_cond_signal(&en->sign.condition);
-
         
+        /* close gate if left open */
+        pthread_mutex_lock(&en->gate.lock);
+        /* if open? lower */
+        if (en->gate.status == 'O') {
+            puts("about to lower gate");
+            en->gate.status = 'L';
+        }
+        pthread_mutex_unlock(&en->gate.lock);
+        pthread_cond_signal(&en->gate.condition);
 
+        //puts("completed cycle");
     }
-
-
-    
 
     return NULL;
 }
