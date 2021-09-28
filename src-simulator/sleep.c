32ac8cdc7ab281154e28081d1ecbd383bab29e05
johnnymadigan <silkjay@icloud.com>
2021-09-28 21:40:43 +1000

    Documentation

--- src-simulator/sleep.c
+++ src-simulator/sleep.c
@@ -1,10 +1,13 @@
-
+/*******************************************************
+ * @file    sleep.c
+ * @author  Johnny Madigan
+ * @date    September 2021
+ * @brief   Source code sleep.h
+ ******************************************************/
 #include <time.h>
 #include "sleep.h"
 
 void sleep_for(int s, int ns) {
-   // for debugging...
-   s = 1;
    long int scale = 1; /* slow down time for debugging, 1 = no change */
 
    struct timespec remaining, requested = {s * scale, ns * scale};
--- src-simulator/sleep.h
+++ src-simulator/sleep.h
@@ -1,3 +1,15 @@
+/*******************************************************
+ * @file    sleep.h
+ * @author  Johnny Madigan
+ * @date    September 2021
+ * @brief   API for temporarily sleeping threads/actions
+ ******************************************************/
 #pragma once
 
+/**
+ * Pause for seconds + nanoseconds.
+ * 
+ * @param seconds
+ * @param nanoseconds
+ */
 void sleep_for(int s, int ns);
