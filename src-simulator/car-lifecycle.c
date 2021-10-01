9cbc926d53756d991a1fabc77636171fa1904fed
johnnymadigan <silkjay@icloud.com>
2021-10-02 00:07:23 +1000

    Testing exit

--- src-simulator/car-lifecycle.c
+++ src-simulator/car-lifecycle.c
@@ -1,6 +1,6 @@
 
-
 #include "queue.h"
+#include <pthread.h>
 
 void *car_lifecycle(void *car) {
     car_t *c = (car_t *)car;
@@ -8,6 +8,8 @@ void *car_lifecycle(void *car) {
     puts("I'M ALIVE");
 
     printf("i %s have been told to goto floor: %d\n", c->plate, c->floor);
+    //push_queue(ex_queues[c->floor], c);
+    //pthread_cond_broadcast(&ex_queues_cond);
     free(c);
     return NULL;
 }
