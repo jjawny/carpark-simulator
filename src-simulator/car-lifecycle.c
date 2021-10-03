26b8273b0dfea710cb483581300159565244d78c
johnnymadigan <silkjay@icloud.com>
2021-10-03 16:59:34 +1000

    Documented

--- src-simulator/car-lifecycle.c
+++ src-simulator/car-lifecycle.c
@@ -1,15 +1,47 @@
+/************************************************
+ * @file    car-lifecycle.c
+ * @author  Johnny Madigan
+ * @date    October 2021
+ * @brief   Source code for car-lifecycle.h
+ ***********************************************/
+#include <stdio.h>          /* for IO operations */
+#include <pthread.h>        /* for mutex locks */
+#include <stdlib.h>         /* for rand */
 
-#include "queue.h"
-#include <pthread.h>
+
+#include "car-lifecycle.h"  /* corresponding header */
+#include "queue.h"          /* for joining exit queue */
+#include "sleep.h"          /* for parking n milliseconds */
+#include "sim-common.h"     /* for the rand lock */
+#include "../config.h"      /* for no. of EXITS */
 
 void *car_lifecycle(void *car) {
     car_t *c = (car_t *)car;
+    int stay = 0;
+    int exit = 0;
 
-    puts("I'M ALIVE");
+    //printf("I %s have been told to goto floor: %d\n", c->plate, c->floor);
 
-    printf("i %s have been told to goto floor: %d\n", c->plate, c->floor);
-    //push_queue(ex_queues[c->floor], c);
-    //pthread_cond_broadcast(&ex_queues_cond);
-    free(c);
+    /* lock rand ONCE here to grab all random values needed 
+    so we can let other threads use rand ASAP */
+    pthread_mutex_lock(&rand_lock);
+    stay = (rand() % 9901) + 100; /* %9901 = 0..9900 and +100 = 100..10000 */
+    exit = rand() % EXITS;
+    pthread_mutex_unlock(&rand_lock);
+
+    /* 10ms drive to parking space,
+    then park for 100..10000ms
+    then 10ms drive to random exit */
+    sleep_for_millis(10);
+    sleep_for_millis(stay); //read LPR before and after + join/leave lvl queue?
+    sleep_for_millis(10);
+
+    /* queue up @ random exit */
+    pthread_mutex_lock(&ex_queues_lock);
+    push_queue(ex_queues[exit], c);
+    pthread_mutex_unlock(&ex_queues_lock);
+    pthread_cond_broadcast(&ex_queues_cond);
+
+    /* thread ends here but car data flow continues to a random exit */
     return NULL;
 }
--- src-simulator/car-lifecycle.h
+++ src-simulator/car-lifecycle.h
@@ -1,3 +1,28 @@
+/************************************************
+ * @file    car-lifecycle.h
+ * @author  Johnny Madigan
+ * @date    October 2021
+ * @brief   API for a car's lifecycle. Only to be
+ *          called by SIMULATE ENTRANCE THREADS when
+ *          letting cars into the carpark. As cars
+ *          move individually once inside, each car
+ *          needs its own thread, especially for
+ *          parking for different times simultaneously.
+ ***********************************************/
 #pragma once
 
+/**
+ * @brief Authorised cars that enter (carpark not full)
+ * are given their own thread so they can drive/park
+ * simultaneously. Once a car is ready to leave, it
+ * will drive to a random exit queuing up to be billed
+ * before leaving the Sim - all times are in milliseconds. 
+ * 
+ * Cars take 10ms to drive to its parking space
+ * Cars park for 100..10000ms
+ * Cars take 10ms to drive to a random exit
+ * 
+ * @param car - the car
+ * @return void* - NULL upon completion
+ */
 void *car_lifecycle(void *car);
