945a7005a4029c470a22b0b0a5e911baf314ff47
johnnymadigan <silkjay@icloud.com>
2021-10-04 14:47:23 +1000

    Updated config

--- config.h
+++ config.h
@@ -1,14 +1,20 @@
-/*******************************************************
+/************************************************
  * @file    config.h
  * @author  Johnny Madigan
  * @date    September 2021
- * @brief   Configuration file so clients can easily recompile 
- *          the software pieces to accomodate different carparks
- ******************************************************/
+ * @brief   Configuration file so clients can easily 
+ *          recompile the software pieces to accommodate
+ *          different carparks
+ ***********************************************/
 #pragma once /* please do not touch this line */
 
+/* 1..5 inclusive for each */
 #define ENTRANCES 5
 #define EXITS 5
 #define LEVELS 5
-#define CAPACITY 20 /* cars per level */
-#define DURATION 120 /* seconds */
+
+/* parking spots per level - must be at least 1 */
+#define CAPACITY 20
+
+/* duration (seconds) each software will run for */
+#define DURATION 20
