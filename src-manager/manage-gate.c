50e0cac7a0f3b28684a784ca238c388d707adc63
johnnymadigan <silkjay@icloud.com>
2021-10-13 10:49:36 +1000

    Gates now handle fire situation

--- src-manager/manage-gate.c
+++ src-manager/manage-gate.c
@@ -18,23 +18,31 @@ void *manage_en_gate(void *args) {
     /* deconstruct args and locate corresponding shared memory */
     args_t *a = (args_t *)args;
     entrance_t *en = (entrance_t *)((char *)shm + a->addr);
+    int opened; /* 0 = no, 1 = yes */
+    
+    /* The fire alarm sys will always set off ALL alarms, so only check 
+    first level as there will always be at least 1 level */
+    int addr = (int)((sizeof(entrance_t) * a->ENS) + (sizeof(exit_t) * a->EXS)); /* offset of where levels begin */
+    level_t *lvl = (level_t *)((char *)shm + addr + (sizeof(level_t) * 0)); /* first level */
 
     while (!end_simulation) {
-        int opened = 0;
+        opened = 0;
 
         /* wait until gate is opened */
         pthread_mutex_lock(&en->gate.lock);
         if (en->gate.status != 'O') pthread_cond_wait(&en->gate.condition, &en->gate.lock);
-        if (en->gate.status != 'O') opened = 1;
+        if (en->gate.status == 'O') opened = 1;
         pthread_mutex_unlock(&en->gate.lock);
 
-        if (opened == 1) sleep_for_millis(20);
+        /* If there's no fire and the gate is opened, keep open for 20ms before lowering */
+        if (!end_simulation && lvl->alarm != '1' && opened) {
+            sleep_for_millis(20);
 
-        pthread_mutex_lock(&en->gate.lock);
-        if (en->gate.status == 'O') en->gate.status = 'L';
-        pthread_mutex_unlock(&en->gate.lock);
+            pthread_mutex_lock(&en->gate.lock);
+            if (en->gate.status == 'O') en->gate.status = 'L';
+            pthread_mutex_unlock(&en->gate.lock);
+        }
     }
-
     return NULL;
 }
 
@@ -43,31 +51,35 @@ void *manage_ex_gate(void *args) {
     /* deconstruct args and locate corresponding shared memory */
     args_t *a = (args_t *)args;
     exit_t *ex = (exit_t *)((char *)shm + a->addr);
+    int opened; /* 0 = no, 1 = yes */
 
+    /* The fire alarm sys will always set off ALL alarms, so only check 
+    first level as there will always be at least 1 level */
+    int addr = (int)((sizeof(entrance_t) * a->ENS) + (sizeof(exit_t) * a->EXS)); /* offset of where levels begin */
+    level_t *lvl = (level_t *)((char *)shm + addr + (sizeof(level_t) * 0)); /* first level */
+    
     while (!end_simulation) {
-        int opened = 0;
+        opened = 0;
 
         /* wait until gate is opened */
         pthread_mutex_lock(&ex->gate.lock);
         if (ex->gate.status != 'O') pthread_cond_wait(&ex->gate.condition, &ex->gate.lock);
-        if (ex->gate.status != 'O') opened = 1;
+        if (ex->gate.status == 'O') opened = 1;
         pthread_mutex_unlock(&ex->gate.lock);
 
-        if (opened == 1) sleep_for_millis(20);
+        /* If there's no fire and the gate is opened, keep open for 20ms before lowering */
+        if (!end_simulation && lvl->alarm != '1' && opened) {
+            sleep_for_millis(20);
 
-        pthread_mutex_unlock(&ex->gate.lock);
-        if (ex->gate.status == 'O') ex->gate.status = 'L';
-        pthread_mutex_unlock(&ex->gate.lock);
+            pthread_mutex_lock(&ex->gate.lock);
+            if (ex->gate.status == 'O') ex->gate.status = 'L';
+            pthread_mutex_unlock(&ex->gate.lock);
+        }
     }
-
     return NULL;
 }
 
 void sleep_for_millis(int ms) {
-    /* FOR DEMONSTRATION & DEBUGGING
-     * slow down time (1 = no change) */
-    long int scale = 1;
-
-    struct timespec remaining, requested = {(ms / 1000) * scale, ((ms % 1000) * 1000000) * scale};
+    struct timespec remaining, requested = {(ms / 1000) * SLOW, ((ms % 1000) * 1000000) * SLOW};
     nanosleep(&requested, &remaining);
 }
